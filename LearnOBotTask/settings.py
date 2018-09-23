42196992e518390cb168ebbcd08ad4db0459c211
Arslan Arshad <arslanarshad07@gmail.com>
2018-09-23 19:44:41 +0500

    Add fiels

--- LearnOBotTask/settings.py
+++ LearnOBotTask/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
-import django_heroku
+from django_heroku import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,8 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
-    # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -161,5 +159,5 @@ STATICFILES_DIRS = (
 #     '/var/www/static/',
 # ]
 
-django_heroku.settings(locals())
+settings(locals())
 
