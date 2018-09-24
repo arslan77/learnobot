91a51a25f582ae109bbf82dfbb650925fa3dcb6a
Arslan Arshad <arslanarshad07@gmail.com>
2018-09-24 07:41:32 +0500

    fix

--- Course/views/CourseViews.py
+++ Course/views/CourseViews.py
@@ -183,7 +183,7 @@ def QuizFinished(request, myQuiz, activeCourseWork):
         mineCourse.course_completed = True
     else:
         mineCourse.percentage = (1.00 - (nextCourseWorks.__len__() / courseWorkList.count())) * 100
-        nextCourseWork = nextCourseWorks[0]
+        nextCourseWork = nextCourseWorks[nextCourseWorks.__len__() - 1]
         mineCourse.current_course_work = nextCourseWork
 
     mineCourse.save()
